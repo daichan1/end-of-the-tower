f92c8d1fbc373c0a76f9089bb55997b331afb22f
daichan1 <chansa-89.one-line@ezweb.ne.jp>
2022-05-20 14:00:37 +0900

    get enemies data

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :players, only: [:index]
+      resources :enemies, only: [:index]
       root "tests#index"
     end
   end
