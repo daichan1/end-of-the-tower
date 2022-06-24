16faba349c3fbb986d7465abaa5efe9243635d05
daichan1 <chansa-89.one-line@ezweb.ne.jp>
2022-06-24 12:09:33 +0900

    create player api test

--- api/spec/factories/card.rb
+++ api/spec/factories/card.rb
@@ -10,4 +10,16 @@ FactoryBot.define do
     action_name { 'strike' }
     execution_count { 1 }
   end
+
+  factory :attacker_card, class: Card do
+    name { 'スコーピオン' }
+    description { '4ダメージを与える' }
+    image_url { 'none' }
+    cost { 0 }
+    card_type { 'アタック' }
+    attack { 4 }
+    defense { 0 }
+    action_name { 'scorpion' }
+    execution_count { 1 }
+  end
 end
---
+++ api/spec/requests/api/v1/players_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'PlayersAPI' do
+  it 'プレイヤーと固有のカードを取得する' do
+    player = create(:attacker)
+    effect_type = create(:effect_type)
+    FactoryBot.create(:attacker_card, player_id: player.id, effect_type_id: effect_type.id)
+
+    get "/api/v1/players/#{player.id}"
+    json = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(json["player"]["name"]).to eq("アタッカー")
+    expect(json["cards"][0]["name"]).to eq("スコーピオン")
+  end
+end
