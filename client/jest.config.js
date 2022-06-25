94a46c213ea0c194a13b93051a74f940f25099bd
daichan1 <chansa-89.one-line@ezweb.ne.jp>
2022-06-25 11:27:58 +0900

    create jest typescript environment

---
+++ client/jest.config.js
@@ -0,0 +1,17 @@
+// import type { Config } from '@jest/types'
+
+const config = {
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy"
+  }
+}
+
+module.exports = config
